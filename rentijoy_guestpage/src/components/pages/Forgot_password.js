0761c66ef96283e95551a787bea6dd8202a54968
Pambali <91451814+Pambali@users.noreply.github.com>
2021-12-16 15:02:18 +0530

    Update Forgot_password.js

--- rentijoy_guestpage/src/components/pages/Forgot_password.js
+++ rentijoy_guestpage/src/components/pages/Forgot_password.js
@@ -5,12 +5,14 @@ import { useNavigate } from 'react-router';
 import Navbar from '../Navbar';
 
  function Forgotpassword(){
+   
      let navigate=useNavigate();
-    function handleSubmit(){  
+    function handleSubmit(e){
+        e.preventDefault();  
        const user_mail=document.getElementById("user_mail");
        const password=document.getElementById("password");
        const confirmpassword=document.getElementById("confirmpassword");
-       Validation(user_mail);
+       
        onEmpty(user_mail)
        onEmpty(password);
        onEmpty(confirmpassword);
@@ -46,12 +48,6 @@ import Navbar from '../Navbar';
           p.nextElementSibling.innerHTML="This field is required";
         }
        }
-
-       function Validation(p){
-        if( (p.value==="")||((!p.value.includes("@")) || (!p.value.includes(".com")))) {
-           p.nextElementSibling.innerHTML="Invalid mail";  
-        }
-     }
   
      function cancle(){
         alert("cancel")
@@ -64,15 +60,16 @@ import Navbar from '../Navbar';
               <Navbar/>
              <div className="forgot">
                  <h2>Forgot password</h2>
+                 <form onSubmit={handleSubmit}>
                  <div className="forgot_password">
                  <div className="formElements">
                   User Email 
-                 <input type="email" name="user_mail" id="user_mail" onChange={onchange}></input>
+                 <input type="email" name="user_mail" id="user_mail" required pattern='^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$' onChange={onchange}></input>
                  <span style={{color:"#f00"}} ></span>
                  </div><br/>
                  <div className="formElements">  
                   Enter New Password 
-                 <input type="password" name="password" id="password" onChange={onchange}></input>
+                 <input type="password" name="password" id="password" required pattern="[A-Z]{1}[a-z]{5,}[0-9]{2}" onChange={onchange}></input>
                  <span style={{color:"#f00"}} ></span>
                  </div><br />
                  <div className="formElements">
@@ -81,13 +78,14 @@ import Navbar from '../Navbar';
                   <span style={{color:"#f00"}} ></span>
                   </div><br/>
                  <div> 
-                 <button className="btn1" onClick={handleSubmit} type="submit">submit</button>
+                 <button className="btn1"  type="submit">submit</button>
                  <button className="btn1" onClick={cancle}>Cancel</button>
                  </div>
                  </div>
+                 </form>
              </div>
              </>
          )
  }
   
- export default Forgotpassword    
+ export default Forgotpassword;
